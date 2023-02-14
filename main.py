9d7b0eb868fbf2c2daa67d6a9f155607275e0de2
Kason <kason.suchow@gmail.com>
2023-02-14 09:04:33 -0700

    Started work on #4, its ugly right now but its WIP

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 import team
 
-team_one = team.Team("Test", "Team A", "AAA")
-team_two = team.Team("Test", "Team B", "BBB")
+teams = {}
 
-home_score = team_one.play_game("home")
-away_score = team_two.play_game("away")
+running = True
 
-print("Final Score", team_one.abbr, home_score, "to", team_two.abbr, away_score)
+def create_team() -> None:
+    in_city = str(input("Enter a City: "))
+    in_name = str(input("Enter a Team Name: "))
+    in_abbr = str(input("Enter a Team Abbreviation: "))
+
+    t = team.Team(in_city, in_name, in_abbr)
+
+    teams[in_abbr] = t
+
+def view_teams() -> None:
+    for team in teams:
+        print(teams[team].city, teams[team].name, teams[team].abbr)
+
+def menu() -> None:
+    global running
+    while running:
+        in_user = int(input("""
+        1. Create Team
+        2. View Teams
+        3. Exit
+        """))
+
+        if in_user == 1:
+            create_team()
+        elif in_user == 2:
+            view_teams()
+        elif in_user == 3:
+            running = False
+        else:
+            raise Exception("Input out of range")
+
+if __name__ == '__main__':
+    menu()
